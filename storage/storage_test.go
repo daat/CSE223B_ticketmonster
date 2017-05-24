3f30439b555c507b794947329a48f8baa3659933
Ming-Lun Cai <daat11elyon@outlook.com>
2017-05-24 14:03:07 -0700

    bug

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -5,9 +5,23 @@ import (
     "runtime/debug"
 	"ticketmonster/storage"
     "fmt"
+    "time"
 )
 
 func TestBackend(t *testing.T) {
+    ne := func(e error) {
+        if e != nil {
+            debug.PrintStack()
+            t.Fatal(e)
+        }
+    }
+
+    as := func(cond bool) {
+        if !cond {
+            debug.PrintStack()
+            t.Fatal("assertion failed")
+        }
+    }
     n := 3
     primaryAddrs := make([]string, n)
     backupAddrs := make([]string, n)
@@ -16,12 +30,21 @@ func TestBackend(t *testing.T) {
         backupAddrs[i] = fmt.Sprintf("localhost:%d", 16000+i)
     }
     backs := make([]storage.PrimaryBackend, n)
-    var e error
     for i := 0; i < n; i++ {
-        e = backs[i].Serve(&storage.BackConfig{PrimaryAddrs: primaryAddrs, BackupAddrs: backupAddrs, This: i})
-        if e != nil {
-            debug.PrintStack()
-			t.Fatal(e)
-        }
+        ne(backs[i].Serve(&storage.BackConfig{PrimaryAddrs: primaryAddrs, BackupAddrs: backupAddrs, This: i}))
     }
+
+    time.Sleep(200 * time.Millisecond)
+
+    bs := storage.BinStorageClient{Backs: primaryAddrs}
+    bs.Init()
+    bin := bs.Bin("0")
+    kv := storage.KeyValue{Key: "uuu", Value: "buy 2"}
+    var succ bool
+    var list storage.List
+    // bin.ListGet(kv.Key, &list)
+    ne(bin.ListAppend(&kv, &succ))
+    bin.ListGet(kv.Key, &list)
+    as(len(list.L) == 1)
+    as(kv.Value == list.L[0])
 }
